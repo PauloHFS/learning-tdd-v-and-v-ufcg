05b9322968927ce4ad31547ddf87407b01a3417b
José Igor <jose.gomes@ccc.ufcg.edu.br>
2024-07-28 22:56:31 -0300

    ingresso: get preco normal ingresso

---
+++ ingressos/src/main/java/com/ufcg/LoteIngressos.java
@@ -0,0 +1,53 @@
+package com.ufcg;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class LoteIngressos {
+
+    private int id;
+    private List<Ingresso> ingressos;
+    private double desconto;
+    private double precoNormal;
+
+    public LoteIngressos(int id, int quantidade_ingressos, double porcentagem_vip, double desconto, double precoNormal) {
+        this.id = id;
+        this.desconto = desconto;
+        this.precoNormal = precoNormal;
+        this.ingressos = new ArrayList<Ingresso>();
+
+        Random random = new Random();
+
+        int quantidade_vip = (int) Math.floor(quantidade_ingressos * porcentagem_vip);
+        int quantidade_meia = (int) Math.floor(quantidade_ingressos * 0.1);
+        int quantidade_normal = quantidade_ingressos - quantidade_meia - quantidade_vip;
+
+        
+        for (int i = 0; i < quantidade_normal; i++) {
+            int id_ingresso = random.nextInt(1000);
+            Ingresso novoIngresso = new Ingresso(id_ingresso, TipoIngresso.NORMAL);
+            ingressos.add(novoIngresso);
+        }
+
+        for (int i = 0; i < quantidade_meia; i++) {
+            int id_ingresso = random.nextInt(1000);
+            Ingresso novoIngresso = new Ingresso(id_ingresso, TipoIngresso.MEIA_ENTRADA);
+            ingressos.add(novoIngresso);
+        }
+
+        for (int i = 0; i < quantidade_vip; i++) {
+            int id_ingresso = random.nextInt(1000);
+            Ingresso novoIngresso = new Ingresso(id_ingresso, TipoIngresso.VIP);
+            ingressos.add(novoIngresso);
+        }
+
+    }
+
+    public double getPrecoIngresso(TipoIngresso tipoIngresso) {
+        if (tipoIngresso == TipoIngresso.NORMAL)
+            return precoNormal * (1 - desconto);
+        return 0;
+    }
+
+}
---
+++ ingressos/src/test/java/LoteIngressosTest.java
@@ -0,0 +1,31 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.ufcg.LoteIngressos;
+import com.ufcg.TipoIngresso;
+
+public class LoteIngressosTest {
+
+    private static LoteIngressos lote;
+
+    @BeforeAll
+    public static void inicializaLote() {
+        int id = 321;
+        int quantidadeIngressos = 500;
+        double porcentagem_vip = 0.2;
+        double desconto = 0.15;
+        double precoNormal = 10;
+
+        lote = new LoteIngressos(id, quantidadeIngressos, porcentagem_vip, desconto, precoNormal);
+    }
+
+    @Test
+    public void getPrecoIngressoNormal() {
+        assertEquals(8.5, lote.getPrecoIngresso(TipoIngresso.NORMAL));
+    }
+
+    
+
+}
